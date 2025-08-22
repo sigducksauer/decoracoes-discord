8ba0bae97403cb6a2cf6fb672b1db60d0e50e601
Jucaskr <Xr36V40q2Z@proton.me>
2025-08-22 04:57:29 -0300

    Update page.jsx

--- src/pages/discuss/page.jsx
+++ src/pages/discuss/page.jsx
@@ -1,5 +1,4 @@
 import { Icons } from "@/components/icons";
-
 import Giscus from "@giscus/react";
 
 export default function Discussion() {
@@ -7,28 +6,23 @@ export default function Discussion() {
     <main className="flex flex-col h-screen text-white">
       <div className="flex items-center gap-2 bg-base-lower p-4 border-b border-border-faint text-icon-tertiary">
         <Icons.forum size="24px" />
-        <p className="font-semibold text-white">Discussions</p>
+        <p className="font-semibold text-white">Discussões</p>
       </div>
       <div className="overflow-auto discord-scrollbar grow">
         <Giscus
           id="comments"
           repo="sigducksauer/decoracoes-discord"
-          repoId="R_kgDOKa2H1A"
+          repoId="R_kgDOPh7ttw"
           mapping="number"
           term="4"
-          reactionsEnabled="0"
+          reactionsEnabled="1"
           emitMetadata="0"
           inputPosition="top"
-          theme={`${
-            import.meta.env.VITE_BASE_IMAGE_URL ||
-            "https://sigducksauer.github.io/decoracoes-discord"
-          }/giscus.css`}
-          lang="en"
+          theme="preferred_color_scheme"
+          lang="pt"
           loading="lazy"
         />
       </div>
     </main>
   );
 }
-
-
